b20f774ca9e71c4dd794a3993b863d993d859def
Marisetty Rajesh <officialtechlock@gmail.com>
2025-08-26 13:00:22 +0530

    Updated app.js with new changes

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.MONGO_URI);
+//console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log(" MongoDB Connected Successfully"))
   .catch(err => console.error(" MongoDB Connection Error:", err.message));
